f9ad16836f946419fa4ccf4f5533c726683f9c11
Steve C <stevencarofano@gmail.com>
2021-08-07 11:54:39 -0400

    added functionality and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,54 +9,99 @@ var myQuestions = [
 	{
 		question: "What does HTML stand for?",
 		answers: {
-			a: 'Happy Turtles Munching Lettuce',
-			b: 'Hypertext Markup Language',
-			c: 'Hypertime Machine Learning'
+			A: 'Happy Turtles Munching Lettuce',
+			B: 'Hypertext Markup Language',
+			C: 'Hypertime Machine Learning'
 		},
-		correctAnswer: 'b'
+		correctAnswer: 'B'
 	},
 	{
 		question: "What does CSS stand for?",
 		answers: {
-			a: 'Cascading Super Savers',
-			b: 'Cats Saving Saucers',
-			c: 'Cascading Style Sheets'
+			A: 'Cascading Super Savers',
+			B: 'Cats Saving Saucers',
+			C: 'Cascading Style Sheets'
 		},
-		correctAnswer: 'c'
+		correctAnswer: 'C'
+	},
+	{
+		question: "What is the common coding practice of typing likeThis called?",
+		answers: {
+			A: 'Monkey Case',
+			B: 'Camel Case',
+			C: 'Apple Case'
+		},
+		correctAnswer: 'B'
 	}
 ];
 
-function startQuiz(){
-    showQuestions();
-    var count = 120;
-    var interval = setInterval(function(){
-  document.querySelector("#timer").innerHTML= count + " second(s) remaining";
-  count--;
-  if (count === 0){
-    clearInterval(interval);
-    alert("You're out of time!");
-  }
-}, 1000);
+function showResults() {
+	document.getElementById("timer").style.display = "none";
+	document.getElementById("submit").style.display = "none";
+	var answerContainers = quizContainer.querySelectorAll('.answers');
+	var userAnswer = '';
+	var numCorrect = 0;
 
+	for (var i = 0; i < myQuestions.length; i++) {
 
-function showQuestions(){
-  // code will go here
-  document.querySelector("#quiz").innerHTML= myQuestions[0];
+		userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
+
+		if (userAnswer === myQuestions[i].correctAnswer) {
+			numCorrect++;
+
+			answerContainers[i].style.color = 'lightgreen';
+		} else {
+			answerContainers[i].style.color = 'red';
+		}
+	}
+	resultsContainer.style.color = "white";
+	resultsContainer.innerHTML = 'You got ' + numCorrect + ' questions correct out of ' + myQuestions.length;
 }
 
-function showResults(){
-  // code will go here
+function startQuiz() {
+	document.getElementById("start").style.display = "none";
+	showQuestions();
+	var count = 120;
+	var interval = setInterval(function () {
+		document.querySelector("#timer").innerHTML = `You have ${count} second(s) remaining`;
+		count--;
+		if (count === 0) {
+			clearInterval(interval);
+			alert("You're out of time!");
+		}
+	}, 1000);
+
+
+	function showQuestions() {
+		var output = [];
+		var answers;
+
+		for (var i = 0; i < myQuestions.length; i++) {
+
+			answers = [];
+
+			for (letter in myQuestions[i].answers) {
+
+				answers.push(
+					'<label>'
+					+ '<input type="radio" name="question' + i + '" value="' + letter + '">'
+					+ letter + ': '
+					+ myQuestions[i].answers[letter]
+					+ '</label>'
+				);
+			}
+
+			output.push(
+				'<div class="question">' + myQuestions[i].question + '</div>'
+				+ '<div class="answers">' + answers.join('') + '</div>'
+			);
+		}
+
+		quizContainer.innerHTML = output.join('');
+	}
+
 }
-
-
-
-	// when user clicks submit, show results
-	document.querySelector("#submit").addEventListener("click", showResults);
-}
-
-
-
-
 
 
 document.querySelector("#start").addEventListener("click", startQuiz);
+document.querySelector("#submit").addEventListener("click", showResults);
